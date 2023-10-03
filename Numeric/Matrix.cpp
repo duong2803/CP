1e24be61ee7a76bf7f8994ef0e1a96759878da64
Nguyen Tung Duong <sieunhan283@gmail.com>
2023-10-03 10:51:14 +0700

    Fix the Matrix.cpp

--- Numeric/Matrix.cpp
+++ Numeric/Matrix.cpp
@@ -3,8 +3,8 @@ struct Matrix{
 	int n, m;
 	T **a;
 	Matrix(int n, int m): n(n), m(m) { a = new T*[n]; for(int i = 0; i < m; ++i){ a[i] = new T[m]; for(int j = 0; j < m; ++j) a[i][j] = 0; } }
-	Matrix(std::initializer_list<std::initializer_list<T>> data) { n = data.size(); m = data.begin()->size(); a = new T*[n]; for(int i = 0; i < m; ++i) a[i] = new T[m]; int i = 0; for(const auto &row: data){ int j = 0; for(const auto &element: row){ a[i][j] = element; ++j;	} ++i; } }
-	Matrix& operator=(std::initializer_list<std::initializer_list<T>> data) { n = data.size(); m = data.begin()->size(); a = new T*[n]; for(int i = 0; i < m; ++i) a[i] = new T[m]; int i = 0; for(const auto &row: data){ int j = 0; for(const auto &element: row){ a[i][j] = element; ++j;	} ++i; } return *this; }
+	Matrix(std::initializer_list<std::initializer_list<T>> data) { n = data.size(); m = data.begin()->size(); a = new T*[n]; for(int i = 0; i < n; ++i) a[i] = new T[m]; int i = 0; for(const auto &row: data){ int j = 0; for(const auto &element: row){ a[i][j] = element; ++j;	} ++i; } }
+	Matrix& operator=(std::initializer_list<std::initializer_list<T>> data) { n = data.size(); m = data.begin()->size(); a = new T*[n]; for(int i = 0; i < n; ++i) a[i] = new T[m]; int i = 0; for(const auto &row: data){ int j = 0; for(const auto &element: row){ a[i][j] = element; ++j;	} ++i; } return *this; }
 	Matrix& operator*=(const Matrix& other){ assert(m == other.n);		Matrix<T> res(n, other.m); for(int i = 0; i < n; ++i){ for(int j = 0; j < other.m; ++j){ for(int k = 0; k < m; ++k){ res[i][j] += a[i][k] * other[k][j]; }	} } *this = res; return *this; }
 	T* operator[](int i){ return a[i]; }
 	const T* operator[](int i) const{ return a[i]; }
